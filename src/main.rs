c19d1aeb95a684554ace4a6c7b7adb4c9e5efbbc
Arman Khan <work.armankhan@gmail.com>
2025-12-14 11:44:22 +0530

    added support for user input, handling invalid commands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("{}: command not found", input.trim())
+        }
+        Err(error) => println!("error: {error}"),
+    }
 }
